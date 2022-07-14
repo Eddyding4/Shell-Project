d6242c94e5b23071735e4753030f33b25b2a9588
Edward Jiaxi Ding <ding291@data.cs.purdue.edu>
2022-07-13 21:49:44 -0400

    \Total:                   0.0 of 70.0\

--- command.cc
+++ command.cc
@@ -108,7 +108,7 @@ void Command::execute() {
       ret = fork();
       if (ret == 0) {
 	int num = (int) _simpleCommands[i]->_arguments.size();
-        char* myargv[num] = (char*)(malloc(sizeof(char));
+        char* myargv[num];
 	for ( int j = 0; j < num; j++ ) {
 	  myargv[j] = _simpleCommands[i]->_arguments[j]->c_str();
 	}
