87a634bf558ecb1cbe721ac2caffcbba9492de34
Edward Jiaxi Ding <ding291@data.cs.purdue.edu>
2022-07-14 16:53:09 -0400

    \Total:                   1.0 of 70.0\

--- command.cc
+++ command.cc
@@ -94,7 +94,7 @@ void Command::print() {
     printf( "\n\n" );
 }
 
-/*void Command::execute() {
+void Command::execute() {
     // Don't do anything if there are no simple commands
     if ( _simpleCommands.size() == 0 ) {
         Shell::prompt();
@@ -184,7 +184,9 @@ void Command::print() {
     clear();
 
     // Print new prompt
+    if ( isatty(0) ) {
     Shell::prompt();
+    }
 }
-*/
+
 SimpleCommand * Command::_currentSimpleCommand;
