6783cb10c43cfb449441678c59a549e0c9dcec40
Edward Jiaxi Ding <ding291@data.cs.purdue.edu>
2022-07-16 16:12:21 -0400

    \Total:                   1.0 of 70.0\

--- command.cc
+++ command.cc
@@ -157,7 +157,7 @@ void Command::execute() {
       FILE *temp = fopen(_errFile, "r");
       char c = fgetc(temp);
       while(c != EOF){
-        fprintf("%s", c);
+        printf("%s", c);
       }
       dup2(fderr, 2);
       close(fderr);
