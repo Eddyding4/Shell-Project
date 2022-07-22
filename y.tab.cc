88cc6215b1b62508b1ac51525fe230543fb1c72b
Edward Jiaxi Ding <ding291@data.cs.purdue.edu>
2022-07-22 12:03:44 -0400

    \test_pipes_redirect_err Failed 0/2\

--- y.tab.cc
+++ y.tab.cc
@@ -1374,7 +1374,7 @@ yyreduce:
       temp += (yyvsp[0].cpp_string)->c_str();  
       temp += " "; 
       if (contains && temp != (yyvsp[0].cpp_string)->c_str()){
-        printf("%s", temp.c_str());
+        Command::_currentSimpleCommand->insertArgument(temp);
         temp.clear();
       } 
     } else {
