c61c48b6aaaf9724090b7a7a8b6915b2671f4352
Edward Jiaxi Ding <ding291@data.cs.purdue.edu>
2022-07-22 10:38:17 -0400

    \test_pipes_redirect_err Failed 0/2\

--- y.tab.cc
+++ y.tab.cc
@@ -1360,7 +1360,7 @@ yyreduce:
   case 3:
 #line 48 "shell.y"
                 {
-    if ((yyvsp[0].cpp_string)->c_str().contains("\"")){
+    if ((yyvsp[0].cpp_string).contains("\"")){
     } 
     printf(" Yacc: insert argument \"%s\"\n", (yyvsp[0].cpp_string)->c_str());
     Command::_currentSimpleCommand->insertArgument( (yyvsp[0].cpp_string) );
