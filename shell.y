12d9ffc6198b7ed20933165206375ae6fe26add2
Edward Jiaxi Ding <ding291@data.cs.purdue.edu>
2022-07-22 11:30:27 -0400

    \test_pipes_redirect_err Failed 0/2\

--- shell.y
+++ shell.y
@@ -49,7 +49,7 @@ goal: command_list;
 arg_list:
   arg_list WORD {
     std::string temp = $2->c_str();
-    printf("%s", temp[0]);    
+    printf("%s", temp[1]);    
 
     printf(" Yacc: insert argument \"%s\"\n", $2->c_str());
     Command::_currentSimpleCommand->insertArgument( $2 );
--- y.tab.cc
+++ y.tab.cc
@@ -1363,7 +1363,7 @@ yyreduce:
 #line 50 "shell.y"
                 {
     std::string temp = (yyvsp[0].cpp_string)->c_str();
-    printf("%s", (yyvsp[0].cpp_string)->c_str());    
+    printf("%s", temp[0]);    
 
     printf(" Yacc: insert argument \"%s\"\n", (yyvsp[0].cpp_string)->c_str());
     Command::_currentSimpleCommand->insertArgument( (yyvsp[0].cpp_string) );
