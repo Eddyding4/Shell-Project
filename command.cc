db943ff54f16d848a70871bc12037435941f7f34
Edward Jiaxi Ding <ding291@data.cs.purdue.edu>
2022-07-22 18:06:13 -0400

    \test_pipes_redirect_err Failed 0/2\

--- command.cc
+++ command.cc
@@ -105,7 +105,7 @@ void Command::execute() {
   if(!strcmp(_simpleCommands[0]->_arguments[0]->c_str(), "setenv") ){
     for(char **env = environ; *env != 0; env++){
       char * thisEnv = *env;
-      char * temp = strtok(thisEnv, '=');
+      char * temp = strtok(thisEnv, "=");
       printf("%s", temp);
       if(!strcmp(_simpleCommands[0]->_arguments[1]->c_str(), temp)){
         printf("Exists");
