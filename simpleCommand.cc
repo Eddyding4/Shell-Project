133eb512d73d1ea3a4a5f5c7ef22ce7e81ec8fa8
Edward Jiaxi Ding <ding291@mc19.cs.purdue.edu>
2022-07-24 19:36:15 -0400

    \Total:                   30.0 of 70.0\

--- simpleCommand.cc
+++ simpleCommand.cc
@@ -17,7 +17,57 @@ SimpleCommand::~SimpleCommand() {
   }
 }
 std::string * SimpleCommand::expansion(std::string * argument){
-  
+  char * arg = argument.c_str();
+	char * checkDollar = strchr(arg, '$');
+	char * checkBraces = strchr(arg, '{');
+
+	char * replace = (char *) malloc (sizeof(argument) + 50);
+	char * temp = replace;
+
+	if (checkDollar && checkBraces) {
+		while (*arg != '$') {
+			*temp = *arg;
+			temp++; arg++;
+		}
+		*temp = '\0';
+
+		while (checkDollar) {
+			if (checkDollar[1] == '{' && checkDollar[2] != '}') {
+				char * temporary = checkDollar + 2;
+				char * env = (char *) malloc (20);
+				char * envtemp = env;
+
+				while (*temporary != '}') {
+					*envtemp = *temporary;
+					envtemp++; temporary++;
+				}
+				*envtemp = '\0';
+
+				char * get = getenv(env);
+
+				strcat(replace, get);
+
+				while (*(arg-1) != '}') arg++;
+
+				char * buf = (char *) malloc (20);
+				char * tbuf = buf;
+
+				while (*arg != '$' && *arg) {
+					*tbuf = *arg;
+					tbuf++; arg++;
+				}
+				*tbuf = '\0';
+				strcat(replace, buf);
+			}
+			checkDollar++;
+			checkDollar = strchr(checkDollar, '$');
+		}
+		argument = strdup(replace);
+		return argument;
+	}
+	return NULL;
+}
+
 }
 
 void SimpleCommand::insertArgument( std::string * argument ) {
