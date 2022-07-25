05e07b1be4037a5d18e1d545a79e9a34e3378d29
Edward Jiaxi Ding <ding291@mc20.cs.purdue.edu>
2022-07-25 11:29:15 -0400

    \Total:                   64.5 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1462,7 +1462,7 @@ YY_RULE_SETUP
   }
   result += str;
   if(strcmp(result.c_str(), "$") == 0){
-	std::string temp = "pidof shell";
+	std::string temp = getpid();
 	yylval.cpp_string = new std::string(temp);
   } else {
     yylval.cpp_string = new std::string(result);
