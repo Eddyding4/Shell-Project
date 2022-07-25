c3e3ba8ad035fe4913eb419ee11c4be3650d8f14
Edward Jiaxi Ding <ding291@mc18.cs.purdue.edu>
2022-07-25 01:36:19 -0400

    \Total:                   64.5 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1462,7 +1462,7 @@ YY_RULE_SETUP
   }
   result += str;
   yylval.cpp_string = new std::string(result);
-  if(strcmp(result, "$") == 0){
+  if(strcmp(result.c_str(), "$") == 0){
 	printf("hello");
   }
   return WORD;
