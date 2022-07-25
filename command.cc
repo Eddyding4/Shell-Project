ee98248033bf38fd0c6e3a2aa81a0aae2f5df946
Edward Jiaxi Ding <ding291@mc20.cs.purdue.edu>
2022-07-24 20:41:46 -0400

    \Total:                   32.5 of 70.0\

--- command.cc
+++ command.cc
@@ -226,67 +226,67 @@ void Command::execute() {
         exit(1);
       }
       if(strcmp(_simpleCommands[i]->_arguments[0]->c_str(), "source") == 0){
-    FILE * fp = fopen(_simpleCommands[i]->_arguments[1]->c_str(), "r");
-    char cmdline [1024];
+        FILE * fp = fopen(_simpleCommands[i]->_arguments[1]->c_str(), "r");
+        char cmdline [1024];
 
-		fgets(cmdline, 1023, fp);
-		fclose(fp);
+        fgets(cmdline, 1023, fp);
+        fclose(fp);
 
-		// save in/out
-		int tmpin=dup(0);
-		int tmpout=dup(1);
+        // save in/out
+        int tmpin=dup(0);
+        int tmpout=dup(1);
 
-		// input command
-		int fdpipein[2];
-		pipe(fdpipein);
-		write(fdpipein[1], cmdline, strlen(cmdline));
-		write(fdpipein[1], "\n", 1);
+        // input command
+        int fdpipein[2];
+        pipe(fdpipein);
+        write(fdpipein[1], cmdline, strlen(cmdline));
+        write(fdpipein[1], "\n", 1);
 
-		close(fdpipein[1]);
+        close(fdpipein[1]);
 
-		int fdpipeout[2];
-		pipe(fdpipeout);
+        int fdpipeout[2];
+        pipe(fdpipeout);
 
-		dup2(fdpipein[0], 0);
-		close(fdpipein[0]);
-		dup2(fdpipeout[1], 1);
-		close(fdpipeout[1]);
+        dup2(fdpipein[0], 0);
+        close(fdpipein[0]);
+        dup2(fdpipeout[1], 1);
+        close(fdpipeout[1]);
 
-		int ret = fork();
-		if (ret == 0) {
-			execvp("/proc/self/exe", NULL);
-			exit(1);
-		} else if (ret < 0) {
-			perror("fork");
-			exit(1);
-	 }
+        int pid = fork();
+        if (pid == 0) {
+          execvp("/proc/self/exe", NULL);
+          exit(1);
+        } else if (ret < 0) {
+          perror("fork");
+          exit(1);
+        }
 
-		dup2(tmpin, 0);
-		dup2(tmpout, 1);
-		close(tmpin);
-		close(tmpout);	
+        dup2(tmpin, 0);
+        dup2(tmpout, 1);
+        close(tmpin);
+        close(tmpout);	
 
-	 char ch;
-	 char * buffer = (char *) malloc (1024);
-	 int r = 0;
+        char ch;
+        char * buffer = (char *) malloc (1024);
+        int r = 0;
 
-				// read output 
-		while (read(fdpipeout[0], &ch, 1)) {
-			if (ch != '\n') { 
-        buffer[r++] = ch;
-      } else {
-        buffer[r++] = '\n';
-      }
-		}
+            // read output 
+        while (read(fdpipeout[0], &ch, 1)) {
+          if (ch != '\n') { 
+            buffer[r++] = ch;
+          } else {
+            buffer[r++] = '\n';
+          }
+        }
 
-		buffer[r] = '\0';
-		printf("%s\n",buffer);
+        buffer[r] = '\0';
+        printf("%s\n",buffer);
 
-		fflush(stdout);
-    clear();
-		Shell::prompt();
-		return;
-    }
+        fflush(stdout);
+        clear();
+        Shell::prompt();
+        return;
+        }
         char** myargv = (char **) malloc ((_simpleCommands[i]->_arguments.size() + 1) * sizeof(char*));
 	      for ( unsigned int j = 0; j < _simpleCommands[i]->_arguments.size(); j++ ) {
 
