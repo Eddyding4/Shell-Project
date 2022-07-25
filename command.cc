00d8c35375b32ec762107c9f1df2ea6c84e7b61b
Edward Jiaxi Ding <ding291@mc20.cs.purdue.edu>
2022-07-24 20:32:48 -0400

    \Total:                   32.5 of 70.0\

--- command.cc
+++ command.cc
@@ -192,7 +192,7 @@ void Command::execute() {
     return;
   }
   if(strcmp(_simpleCommands[i]->_arguments[0]->c_str(), "source") == 0){
-    FILE * fp = fopen(_simpleCommands[i]->_arguments[1]->c_str(), O_RDWR);
+    FILE * fp = fopen(_simpleCommands[i]->_arguments[1]->c_str(), "r");
     char cmdline [1024];
 
 		fgets(cmdline, 1023, fp);
