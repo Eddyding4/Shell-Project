c3d95cdbacb8678f5e6175231f36eb6810419482
Edward Jiaxi Ding <ding291@mc20.cs.purdue.edu>
2022-07-25 11:21:57 -0400

    \Total:                   64.5 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1461,20 +1461,21 @@ YY_RULE_SETUP
     found = str.find('$');
   }
   result += str;
-  if(strcmp(result.c_str(), "$") == 0){
+  /*if(strcmp(result.c_str(), "$") == 0){
 	std::string temp = std::to_string(getpid());
 	printf("%s", temp.c_str());
 	yylval.cpp_string = new std::string(result);
   } else {
     yylval.cpp_string = new std::string(result);
-  }
+  }*/
+  yylval.cpp_string = new std::string(result);
 
   return WORD;
 }
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 249 "shell.l"
+#line 250 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   yylval.cpp_string = new std::string(yytext);
@@ -1483,10 +1484,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 257 "shell.l"
+#line 258 "shell.l"
 ECHO;
 	YY_BREAK
-#line 1490 "lex.yy.cc"
+#line 1491 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(SOURCE_SC):
 	yyterminate();
@@ -2504,6 +2505,6 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 257 "shell.l"
+#line 258 "shell.l"
 
 
