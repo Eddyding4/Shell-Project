a0c5b2dc564514e7c5d79dc6c398964c626df7d4
Edward Jiaxi Ding <ding291@mc21.cs.purdue.edu>
2022-07-26 13:46:05 -0400

    \Total:                   58.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1376,7 +1376,6 @@ YY_RULE_SETUP
 #line 156 "shell.l"
 {
 	//subshell
-	//setenv("SHELL", "/usr/local/bin/bash", 1);
   	std::string * cmd = new std::string(yytext);
 	cmd->pop_back();
 
@@ -1443,7 +1442,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 223 "shell.l"
+#line 222 "shell.l"
 {
 	//tilde stuff
   std::string str = std::string(yytext);
@@ -1465,7 +1464,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 242 "shell.l"
+#line 241 "shell.l"
 {
   //environment expansion
   std::string str = std::string(yytext);
@@ -1499,7 +1498,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 274 "shell.l"
+#line 273 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   yylval.cpp_string = new std::string(yytext);
@@ -1508,10 +1507,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 282 "shell.l"
+#line 281 "shell.l"
 ECHO;
 	YY_BREAK
-#line 1515 "lex.yy.cc"
+#line 1514 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(SOURCE_SC):
 	yyterminate();
@@ -2529,6 +2528,6 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 282 "shell.l"
+#line 281 "shell.l"
 
 
