164586bc31b40dae7997d32aacc37dd4130787e4
Edward Jiaxi Ding <ding291@mc21.cs.purdue.edu>
2022-07-25 22:28:05 -0400

    \Total:                   66.0 of 70.0\

--- command.cc
+++ command.cc
@@ -230,6 +230,7 @@ void Command::execute() {
       }
         char** myargv = (char **) malloc ((_simpleCommands[i]->_arguments.size() + 1) * sizeof(char*));
 	      for ( unsigned int j = 0; j < _simpleCommands[i]->_arguments.size(); j++ ) {
+          printf("%s", _simpleCommands[i]->_arguments[j]->c_str());
           if (!strcmp(_simpleCommands[i]->_arguments[j]->c_str(), "?")){
             printf("hELO");
             int status;
