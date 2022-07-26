737c5ff3034c4b04484571b951fe8430d5e46955
Edward Jiaxi Ding <ding291@mc21.cs.purdue.edu>
2022-07-26 14:15:40 -0400

    \Total:                   58.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1466,6 +1466,7 @@ case 17:
 YY_RULE_SETUP
 #line 241 "shell.l"
 {
+	printf("Hello");
   //environment expansion
   std::string str = std::string(yytext);
   std::string result = "";
@@ -1499,7 +1500,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 274 "shell.l"
+#line 275 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   yylval.cpp_string = new std::string(yytext);
@@ -1508,10 +1509,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 281 "shell.l"
+#line 282 "shell.l"
 ECHO;
 	YY_BREAK
-#line 1515 "lex.yy.cc"
+#line 1516 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(SOURCE_SC):
 	yyterminate();
@@ -2529,6 +2530,6 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 281 "shell.l"
+#line 282 "shell.l"
 
 
